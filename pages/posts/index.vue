afe25159591210a36c87e2f2aafb5f13ea1dabe4
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-19 00:16:03 +0800

    Using Promises in asyncData

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,16 +9,23 @@ export default {
     components: {
         PostList,
     },
-    asyncData(context, callback) {
-      setTimeout(() => {
-        callback(new Error(), {
-          loadedPosts:[
-            { id: '1', title: 'Post 1', previewText: 'Post 1', thumbnail: 'https://lorempixel.com/400/200/cats/1'},
-            { id: '2', title: 'Post 2', previewText: 'Post 2', thumbnail: 'https://lorempixel.com/400/200/cats/2'},
-            { id: '3', title: 'Post 3', previewText: 'Post 3', thumbnail: 'https://lorempixel.com/400/200/cats/3'},
-          ]
-        });        
-      }, 1000);
+    asyncData(context) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({
+            loadedPosts:[
+              { id: '1', title: 'Post One', previewText: 'Post 1', thumbnail: 'https://lorempixel.com/400/200/cats/1'},
+              { id: '2', title: 'Post Two', previewText: 'Post 2', thumbnail: 'https://lorempixel.com/400/200/cats/2'},
+              { id: '3', title: 'Post Three', previewText: 'Post 3', thumbnail: 'https://lorempixel.com/400/200/cats/3'},
+            ]
+          });
+        // reject(new Error());        
+        }, 1000);
+      }).then((data) => {
+          return data;
+      }).catch((error) => {
+          context.error(error);
+      });
     },
 };
 </script>
