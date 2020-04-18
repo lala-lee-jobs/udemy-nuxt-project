19e32f714884e03f705c76698c00e1e84b0dfedc
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-18 12:28:38 +0800

    Creating an Admin Section

---
+++ pages/admin/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="admin-page">
+        <section class="new-post">
+            <button @click="onCreate">Create Post</button>
+        </section>
+        <section class="existing-posts">
+            <h1>Existing Posts</h1>
+            <PostList />
+        </section>
+    </div>
+</template>
+<script>
+import PostList from '@/components/Posts/PostList';
+
+export default {
+    components: {
+        PostList
+    },
+    methods: {
+      onCreate() {
+        this.$router.push('/admin/new-post');
+      },
+    },
+};
+</script>
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+</style>
