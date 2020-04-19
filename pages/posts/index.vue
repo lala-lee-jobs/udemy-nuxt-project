8cd805106f8e203fd7a0cd937c9738c407acb425
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-20 00:09:02 +0800

    在Vuex使用nuxtServerInit

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,24 +9,6 @@ export default {
     components: {
         PostList,
     },
-    fetch(context) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve({
-            loadedPosts:[
-              { id: '1', title: 'Post One', previewText: 'Post 1', thumbnail: 'https://lorempixel.com/400/200/cats/1'},
-              { id: '2', title: 'Post Two', previewText: 'Post 2', thumbnail: 'https://lorempixel.com/400/200/cats/2'},
-              { id: '3', title: 'Post Three', previewText: 'Post 3', thumbnail: 'https://lorempixel.com/400/200/cats/3'},
-            ]
-          });
-        // reject(new Error());        
-        }, 1000);
-      }).then((data) => {
-          context.store.commit('setPosts', data.loadedPosts);
-      }).catch((error) => {
-          context.error(error);
-      });
-    },
     computed: {
       loadedPosts() {
         return this.$store.getters.loadedPosts;
--- store/index.js
+++ store/index.js
@@ -11,6 +11,18 @@ const createStore = () => {
             },
         },
         actions: {
+            nuxtServerInit(vueContext, context) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                      vueContext.commit('setPosts',[
+                          { id: '1', title: 'Post One', previewText: 'Post 1', thumbnail: 'https://lorempixel.com/400/200/cats/1'},
+                          { id: '2', title: 'Post Two', previewText: 'Post 2', thumbnail: 'https://lorempixel.com/400/200/cats/2'},
+                          { id: '3', title: 'Post Three', previewText: 'Post 3', thumbnail: 'https://lorempixel.com/400/200/cats/3'},
+                      ]);
+                      resolve();
+                    }, 1000);                   
+                });
+            },
             setPosts(vueContext, posts) {
                 vueContext.commit('setPosts', posts);
             },
