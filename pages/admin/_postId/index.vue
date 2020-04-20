46643667925a8eb6a40ce27998bec6d048ab1a64
lala.lee <lala.lee.jobs@gmail.com>
2020-04-20 15:33:41 +0800

    Time to Practice - Fetching Data

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -7,21 +7,22 @@
 </template>
 <script>
 import AdminPostForm from '@/components/Admin/AdminPostForm';
+import axios from 'axios';
 
 export default {
   layout:'admin',
   components: {
       AdminPostForm
   },
-  data() {
-      return {
-          loadedPost: {
-              author: 'lala',
-              title: 'My awesome post',
-              content : 'lorem text',
-              thumbnailLink: 'https://lorempixel.com/400/200/cats/4',
-          },
-      };
+  asyncData(context) {
+    const { postId } = context.params;
+    return axios
+      .get(`https://udemy-nuxt-demo.firebaseio.com/posts/${postId}.json`)
+      .then((res) => {
+        return {
+          loadedPost : res.data,
+        };
+      }).catch(e => context.error(e));
   },
 };
 </script>
