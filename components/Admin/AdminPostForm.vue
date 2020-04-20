5e39e276183452056d6d56faea16904caf24dd38
lala.lee <lala.lee.jobs@gmail.com>
2020-04-20 14:55:55 +0800

    Initializing our Store

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -2,8 +2,9 @@
   <form @submit.prevent="onSave">
     <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-    <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+    <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
     <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+    <AppControlInput control-type="textarea" v-model="editedPost.previewText">previewText</AppControlInput>
     <AppButton type="submit">Save</AppButton>
     <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
   </form>
@@ -29,8 +30,9 @@ export default {
             : {
               auth: '',
               title: '',
-              thumbnailLink: '',
+              thumbnail: '',
               content: '',
+              previewText: '',
             },
       }
   },
